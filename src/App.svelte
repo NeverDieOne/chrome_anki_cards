9d638215188e80935c38efda85c990d91303c438
neverdieone <altiore@mail.ru>
2022-10-12 12:08:26 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+  import { onMount } from 'svelte';
+  // @ts-ignore
+  import axios from 'axios';
+
+  const ankiApi = 'http://127.0.0.1:9999'
+  let userName = localStorage.getItem('tgNickname')
+  $: userDecks = []
+  let selected = localStorage.getItem('currentDeckId')
+
+  async function updateDecks() {
+    let response = await axios.get(`${ankiApi}/anki/${userName}/decks`)
+    userDecks = response.data
+  }
+
+  onMount(async () => {
+    await updateDecks()
+  })
+
+  function saveTelegramNickname(event) {
+    let inputElement = document.getElementById('tgNickname')
+    // @ts-ignore
+    userName = inputElement.value
+    localStorage.setItem('tgNickname', userName)
+    updateDecks()
+  }
+
+  function changeDeckId(event) {
+    localStorage.setItem('currentDeckId', event.target.value)
+  }
+</script>
+
+<main>
+  <div class="container">
+    <div class="input-group mb-3 mt-5">
+      <label class="input-group-text" for="inputGroupSelect01">Колода</label>
+      <select on:change={changeDeckId} class="form-select">
+        {#each userDecks as deck}
+          {#if selected == deck.id}
+            <option selected value="{deck.id}">{deck.name}</option>
+          {:else}
+            <option value="{deck.id}">{deck.name}</option>
+          {/if}
+        {/each}
+      </select>
+    </div>
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Введи ник в ТГ" id="tgNickname" value="{userName}">
+      <button class="btn btn-outline-secondary" type="button" id="button" on:click={saveTelegramNickname}>Сохранить</button>
+    </div>
+  </div>
+</main>
